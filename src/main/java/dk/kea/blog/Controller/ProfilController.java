ce84891d6f97000e74a81db701f24e73ffcb1f38
JoachimGoodman <joac1087@stud.kea.dk>
2019-03-11 08:57:35 +0100

    added profil 2

---
+++ src/main/java/dk/kea/blog/Controller/ProfilController.java
@@ -0,0 +1,35 @@
+package dk.kea.blog.Controller;
+
+import dk.kea.blog.Models.User;
+import dk.kea.blog.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class ProfilController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping(value="/profil")
+    public String changePassword(@ModelAttribute(name="User") User user, Model model, HttpSession session) {
+
+        session.setAttribute("password", user.getPassword());
+
+        System.out.println(user.getPassword());
+
+        if(userService.changePassword(user)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+        return "profil";
+    }
+
+}
